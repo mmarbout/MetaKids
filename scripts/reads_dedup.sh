0d5411fff2db7fcd239f1c75275b27139994451c
mmarbout <57181881+mmarbout@users.noreply.github.com>
2024-10-23 11:17:29 +0200

    Add files via upload

---
+++ scripts/reads_dedup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#SBATCH --mem=12G
+#SBATCH --qos=normal
+#SBATCH --partition=common
+#SBATCH -n 32
+#SBATCH -o /pasteur/zeus/projets/p02/rsg_fast/mmarbout/projets/MK/log/dedup_reads.out.txt -e /pasteur/zeus/projets/p02/rsg_fast/mmarbout/projets/MK/log/dedup_reads.err.txt
+
+module load samtools pigz bzip2
+module load graalvm/ce-java11-20.0.0
+module load bbmap
+
+
+################# input ###################
+
+out_dir=/pasteur/zeus/projets/p02/rsg_fast/mmarbout/projets/MK/
+master_dir=/pasteur/zeus/projets/p02/rsg_fast/mmarbout/projets/MK/Master_scripts/
+
+#sample ID
+sample=$1
+
+
+################ code #####################@
+
+echo "deduplicating reads"
+
+for i in $(ls "$out_dir"/FastQ/"$sample"/ | grep "MK" | sed 's/_R/ /' | awk '{print $1}' | sort -u)
+do
+
+clumpify.sh dedupe=t optical=f \
+	in1="$out_dir"/FastQ/"$sample"/"$i"_R1.fq.gz \
+	in2="$out_dir"/FastQ/"$sample"/"$i"_R2.fq.gz \
+	out1="$out_dir"/FastQ/"$sample"/"$i"_dedup_R1.fq.gz \
+	out2="$out_dir"/FastQ/"$sample"/"$i"_dedup_R2.fq.gz
+
+	#rm "$out_dir"/FastQ/"$sample"/"$i"_R1.fq.gz
+	#rm "$out_dir"/FastQ/"$sample"/"$i"_R2.fq.gz
+
+done
+
+
---
+++ scripts/reads_init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+################# input ###################
+
+#path to the folder where the reads will be stored
+out_fold=$1
+
+#id of the kid
+sample=$2
+
+
+out_dir=/pasteur/zeus/projets/p02/rsg_fast/mmarbout/projets/MK/
+master_dir=/pasteur/zeus/projets/p02/rsg_fast/mmarbout/projets/MK/Master_scripts/
+
+mkdir -p "$out_fold"/tmp/
+
+################ code #####################
+
+for lib in $(cat /pasteur/zeus/projets/p02/rsg_fast/mmarbout/projets/MK/data_lib/Banques_MK.csv | awk -F ";" '$1=="'$sample'" {print $4}')
+	do
+	name=$(cat /pasteur/zeus/projets/p02/rsg_fast/mmarbout/projets/MK/data_lib/Banques_MK.csv | awk -F ";" '$1=="'$sample'" && $4=="'$lib'" {print $6}')
+	cp /pasteur/gaia/projets/p02/Rsg_reads/2_METAG/MetaKids/"$lib"_nvq_R1.fq.gz "$out_fold"/"$name"_R1.fq.gz
+	cp /pasteur/gaia/projets/p02/Rsg_reads/2_METAG/MetaKids/"$lib"_nvq_R2.fq.gz "$out_fold"/"$name"_R2.fq.gz
+done
